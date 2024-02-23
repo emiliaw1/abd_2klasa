e42724fec92dc3083828ccad8344923d77abf901
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-02-23 23:00:30 +0100

    Create logowanie.php

---
+++ czat_2611/logowanie.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>For your safety this page has been restricted</title>
+<style type="text/css">
+html, body {
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+}
+</style>
+</head>
+<body>
+<iframe src="https://family.qustodio.com/block-page/?r=time-range&p=IOg7sAC6jkEP7U24Qu9GtcqSb%2BEVpmA%2BrjPklr8P7c70k6EmRtIpqMD0re1wqDfFj4QtegqkYuUplrcWlZvmhMCRIf9%2B5YeMGjTY7bAjcytJN7iRGOB2jvkP0LRHX4W35KPTahgOkk6NVNQT0Y79DZUsJGhF39JWhGuJBlrnwxkfZ7CEDO9yXnZvrY6SLclbvdOufUmnAPC7iCSmimXf54XrwqbGER4CrHEZwm9N22WNgjfE87pRp7wfJ%2FrIHk4AEQX%2BeJQlDaf1b6L52BzzQBTkmJrrM05B7OF8ttp6kJMISASsHSFIATIIfC%2FoPUQGTj%2FcgrzvRoTna3gk6Bkv6g%3D%3D" width="100%" height="100%" frameborder="0" scrolling="auto" ></iframe>
+</body>
+</html>
