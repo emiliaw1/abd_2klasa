afd5f9434b66112571bcf81db7f0a3f0810b137e
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-06-06 11:35:58 +0200

    Add files via upload

---
+++ formularz.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dane osobowe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="baner">
+        <h1>Dane osobowe pracowników</h1>
+    </header>
+    <main class="bloki">
+        <section class="blok_lewy">
+            <ol>
+                <a href="index.php">Wyświetl dane</a>
+                <a href="formularz.php">Wpisz dane</a>
+            </ol>
+        </section>
+        <section class="blok_prawy">
+            <h2>Podaj dane</h2>
+            <form action="">
+                Nazwisko: <input type="text"> <br/>
+                Imię: <input type="text"> <br/>
+                PESEL: <input type="number"> <br/>
+                <input type="submit" value="Sprawdź i zapisz">
+            </form>
+        </section>
+    </main>
+    <footer class="stopka">
+        <h5>Autor strony: shzjwshskdnajdj</h5>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dane osobowe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="baner">
+        <h1>Dane osobowe pracowników</h1>
+    </header>
+    <main class="bloki">
+        <section class="blok_lewy">
+            <ol>
+                <a href="index.php">Wyświetl dane</a>
+                <a href="formularz.php">Wpisz dane</a>
+            </ol>
+        </section>
+        <section class="blok_prawy">
+            <h2>Dane osobowe</h2>
+        </section>
+</main>
+    <footer class="stopka">
+        <h5>Autor strony: shzjwshskdnajdj</h5>
+    </footer>
+</body>
+</html>
